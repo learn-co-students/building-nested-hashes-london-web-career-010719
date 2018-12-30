c7683f27d576da0b1b32c0c30b9217442eaca4ca
parik panchal <parikspanchal@icloud.com>
2018-12-30 20:13:05 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,7 +22,7 @@ def fourth_challenge
       :heroine_friends => [
         {name: "Steven",
           age: "30",
-          attitude: "confused"    
+          attitude: "confused"
         },
         {name: "Nurse",
           age: "44",
